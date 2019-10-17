8acf546c54ce50295a626c63243cbc63c752bd2b
Lê Văn Uy <50730042+uykte2019@users.noreply.github.com>
2019-10-17 20:31:17 +0700

    Add files via upload

---
+++ PageListViewHoa/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using Xamarin.Forms.Xaml;
+
+namespace PageListViewHoa
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<hoa> Hoas = new ObservableCollection<hoa>();
+        public MainPage()
+        {
+            InitializeComponent();
+            Hoas.Add(new hoa { TenHoa = "Đón xuân", Gia = 50000, Hinh = "cuc1.jpg", Mota = "Hoa cúc các màu: trắng, vàng, cam" });
+            Hoas.Add(new hoa { TenHoa = "Hồn nhiên", Gia = 60000, Hinh = "honnhien1.jpg", Mota = "Hoa cúc vàng, cam. lá măng" });
+            Hoas.Add(new hoa { TenHoa = "Tím thủy chung", Gia = 45000, Hinh = "cuctim.jpg", Mota = "Hoa cúc tím" });
+            lsvHoa.ItemsSource = Hoas;
+        }
+    }
+}
---
+++ PageListViewHoa/hoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PageListViewHoa
+{
+    class hoa
+    {
+        public string TenHoa { get; set; }
+        public string Hinh { get; set; }
+        public double Gia { get; set; }
+        public string Mota { get; set; }
+    }
+}
